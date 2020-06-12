6e39fc86a995887a2a1ec36e90b3358faf86ddb1
Your Name <you@example.com>
2020-06-12 10:19:41 -0400

    messed around with css

--- react-nutshell/src/Components/news/NewsPage.js
+++ react-nutshell/src/Components/news/NewsPage.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import './NewsPage.css'
 
 
 class NewsPage extends Component {
@@ -9,17 +10,18 @@ class NewsPage extends Component {
         <>
       <div className="card">
         <div className="card-content">
-    <p>{this.props.news.date}</p>
+    <p className="time">  {this.props.news.date}</p>
           <h3>
             <span className="articleTitle">{this.props.news.title}</span>
           </h3>
           <p>{this.props.news.synopsis}</p>
           <p>{this.props.news.url}</p>
-        </div>
-      </div>
+     
       <button type="button" onClick={() => this.props.deleteArticle(this.props.news.id)}>Delete</button>
       <button type="button"
         onClick={() => {this.props.history.push(`/news/${this.props.news.id}/edit`)}}>Edit</button>
+           </div>
+      </div>
       </>
     );
     
